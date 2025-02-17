a2c2cad8e8c7f43498c63f511885dd88848d2759
C42xe2 <erikdonath@proton.me>
2025-02-17 10:10:56 +0100

    Removed Debug

--- main.ts
+++ main.ts
@@ -2,30 +2,29 @@ import { loadMathJax, Plugin } from 'obsidian';
 
 export default class MathPlugin extends Plugin {
 	async onload() {
-		console.log("Math plugin loaded!");
+		console.log("Math plugin loaded");
 
 		await loadMathJax();
 
 		if (!MathJax) {
-		  console.warn("MathJax was not defined despite loading it.");
+		  console.warn("MathJax was not defined despite loading it");
 		  return;
 		}
 
 		// Load preample
-		const preamble = "$\\newcommand{\\Vec}[3]{\\mathbf{\\begin{pmatrix} #1 \\\\ #2 \\\\ #3 \\end{pmatrix}}}$";
+		const preamble = "\\newcommand{\\Vec}[3]{\\mathbf{\\begin{pmatrix} #1 \\\\ #2 \\\\ #3 \\end{pmatrix}}}";
 		if (MathJax.tex2chtml == undefined) {
 			MathJax.startup.ready = () => {
 				MathJax.startup.defaultReady();
 				MathJax.tex2chtml(preamble);
-				console.log("Preamble loaded")
 			};
 		} else {
 			MathJax.tex2chtml(preamble);
-			console.log("Preamble loaded")
 		}
 	}
 
 	onunload() {
-		console.log("Math plugin unloaded!");
+		console.warn("Can't disable preamble")
+		console.log("Math plugin unloaded");
 	}
 }
